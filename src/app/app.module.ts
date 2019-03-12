f13b3dbfa0053d52b50da6237f3711f1afeb5deb
zh-konopleova <konopleova@gmail.com>
2019-03-12 18:42:34 +0300

    Add search & history components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
