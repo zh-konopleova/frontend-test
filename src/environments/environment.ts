a097884c5eb31bb301320d3730c85d8b5bc12e62
zh-konopleova <konopleova@gmail.com>
2019-03-22 09:57:38 +0300

    edit .gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+
+  firebase: {
+    apiKey: "AIzaSyAF5FUhQw6pXhjkEY7PmZuKWvnUzLRQito",
+    authDomain: "frontend-test-883fe.firebaseapp.com",
+    databaseURL: "https://frontend-test-883fe.firebaseio.com",
+    projectId: "frontend-test-883fe",
+    storageBucket: "frontend-test-883fe.appspot.com",
+    messagingSenderId: "218601180874"
+  }
+};
